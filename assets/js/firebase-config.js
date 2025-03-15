d5d99eb67c09a058a44b0179d964214b0329631d
Ben Burney <mrburnification@gmail.com>
2025-03-15 11:48:40 +1100

    added export

--- assets/js/firebase-config.js
+++ assets/js/firebase-config.js
@@ -7,4 +7,6 @@ const firebaseConfig = {
     messagingSenderId: "671875277305",
     appId: "1:671875277305:web:0e3f0b53cd27dfb2bd5d28",
     measurementId: "G-4BCZLHFWGQ"
-  };
+};
+
+export default firebaseConfig;
